e4d9616bcc650a81a85f25f5df124e8bfdde4b48
Rishabh Gulati <gulati.r7121@gmail.com>
2023-07-25 06:23:34 +0530

    Create first_class.py
    class first

---
+++ first_class.py
@@ -0,0 +1,35 @@
+print(" Hello!! esaie ")
+# |      --->   (our task which has to be completed)
+# is a command (hey computer print something for me)
+# is used to comment things (these things will be ignored by computer)
+# for example I want to explain to a non programmer /
+
+# this line will print Hello Robot
+# print("Hello RObot")
+# print(10.0)
+# print(200)
+
+# # DATA types 
+# 1. Integer --> numbers from - infinity --> +infinity
+#                0,-1,-500,14,53......(all numbers)
+
+# # to use them ( we dont require " ")
+# 2. STRING --> alphabets and characters
+#               "R","e","circus","batman","flash#!@",
+#            # to use them we have to rquire ( " ")
+# print("EAse")
+# print("Call of duty")
+# print("GOat$$$#%")
+
+
+# 3. FLOAT -->numbers with decimals
+#             100.1,5.2,333.333,0.999,1.11
+#     # we also dont require " "
+# print(100.1)
+# print(2.333)
+print("100"+100)# you are trying to do a crime
+# by combining string and integer
+# now numbers are being treated as strings
+# "ch" +"at" ==> chat
+#100 +100 ==> 200
+# "100" +"100" ==> 100100
