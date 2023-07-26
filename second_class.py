0e57037546b1a5977ea51577e450d0b605207593
Rishabh Gulati <gulati.r7121@gmail.com>
2023-07-26 06:24:05 +0530

    Create second_class.py

---
+++ second_class.py
@@ -0,0 +1,28 @@
+# e32 = 300
+# # it is not valid because we cant start names from numbers of variables
+# print(e32)
+# name = "King"
+# # print(name)
+# a = 100
+# b = 200
+# print(a+b) # + sign is there to add
+# print(a-b) #- sign is there to subtract
+# print(a/b) # / sign is there to divide
+# print(a*b) # * is there to multiply
+# print(a**b)## ** is there to power ( 2**4 ==2*2*2*2)
+# # 100 *100 *100 *100 ---------------200 times
+# print(a%b) # it gives the remainder when we divide 100 by 200
+
+# first_name = "100"
+# last_name = "200"
+# print(first_name+last_name) # it is taking them as a string
+
+# a = 100 # I want anyone can give the values --> insert / input
+# b = 300
+# print(a+b)
+
+# by default, input is set to take strings as input
+# we have to convert them into integers
+a = int(input("Enter number for a : "))
+b = int(input("Enter number for b : "))
+print(a+b)
