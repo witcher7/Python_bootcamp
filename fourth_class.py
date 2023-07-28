1cc51ae43b19c86a033094fda27c9ce3153e5a4e
Rishabh Gulati <gulati.r7121@gmail.com>
2023-07-28 06:27:56 +0530

    Create fourth_class.py

---
+++ fourth_class.py
@@ -0,0 +1,21 @@
+# import random  # we are importing random book from library
+# ans = random.randint(1,4)
+# print(ans)
+# in your game
+
+# how we can if number is divisible by 7 --> 
+
+n = 10
+# if n ==10:
+#   print("hi")
+# elif n == 20:
+#   print("HOLA")
+# else: 
+
+
+if n ==10:
+  print("Hi")
+if n ==20:
+  print("Hey")
+if n ==30
+ print("Hola")
