654e43156553ff69104f1fc7f7dd47db864a120a
Rishabh Gulati <gulati.r7121@gmail.com>
2023-08-03 06:20:55 +0530

    Create seventh_class.py

---
+++ seventh_class.py
@@ -0,0 +1,29 @@
+n = 1
+while n <=10:
+  print(n)
+  n = n +1
+
+print numbers from 150 --> 50
+
+Q1. Write a program to print the following using while loop
+a. First 10 Even numbers
+b. First 10 Odd numbers
+
+n = 2
+while n <=20:
+  print(n)
+  n=n+2
+
+
+Write a program to print first 10 integers and their squares using while loop.
+1 1
+2 4
+3 9 and so on
+
+n = 1
+while n <=10:
+  print(n*n) # squares
+  n =n +1
+
+Write for loop statement to print the following series:
+10, 20, 30 … … 300
