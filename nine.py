784b0fccbc57298a9b95bfe8b72a81a736617a01
Rishabh Gulati <gulati.r7121@gmail.com>
2023-08-09 06:43:26 +0530

    Create nine.py

---
+++ nine.py
@@ -0,0 +1,38 @@
+# Hey Buddy !!!....I have a sore throat today,,, I can't speak, so please
+# cooperate with me for this class ??? okay ? Esale ???
+
+# buddy do yo have any doubts till now?
+
+#Printing all letters except some using input from the user
+
+# --> enter input for the letter --> Nike
+# --> which element you dont want to print --> i
+#   N
+#   k
+#   e
+
+# functions ---> these are piece of code we make so that it can be
+# used again and again
+# normally it will not work until and unless we have called it
+
+# def jerry():
+#   print("Jerry")
+
+# jerry() # name of the function 
+# print("Hi")
+# a = 10
+# b = 20
+# print(a+b)
+# jerry()
+
+def sum(a,b):
+  ans = a +b
+  return ans # we use return to return a value back to our main world
+
+a = int(input("Enter number for a : "))
+b = int(input("Enter number for b : "))
+koffi = sum(a,b) # ans is ready to take the value from return 
+print(koffi)
+
+# make functions for them as well
+# do this for mul(multiplication) ,div ( division) , sub(subtraction)
