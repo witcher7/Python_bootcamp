e06923c209868ba3ea64bafb8234c1ed8d8ace26
Rishabh Gulati <gulati.r7121@gmail.com>
2023-08-15 06:21:07 +0530

    Update fourth_class.py

--- fourth_class.py
+++ fourth_class.py
@@ -19,3 +19,32 @@ if n ==20:
   print("Hey")
 if n ==30
  print("Hola")
+
+# let's now learn about lists
+# if I want to store multiple items at once place, I will use list
+name = "Python"
+print(name)
+
+namelist = [ "Python" ,"Java","Devops","Cloud","C++"]
+#               0         1       2      3       4
+# index numbers in list  
+# print(namelist)
+# namelist.append(100)
+# print(namelist) # append adds the element in the last
+# namelist.append("AWS")
+# print(namelist)
+# namelist.pop() # pop will remove last element or specify the index num
+# print(namelist)
+print(namelist)
+# If I want to print elements one by one
+
+# length= len(namelist) # this is how we get know length of list
+# print(length)
+# for i in range(length-1,-1,-1):
+#    print(namelist[i])
+
+list = [10,35,41,100,30]
+# print only even elements in the list
+# 10
+# 100
+# 30
