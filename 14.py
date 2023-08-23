51066c6acb2e7529284ef7b245a6a85cf5c40b7d
Rishabh Gulati <gulati.r7121@gmail.com>
2023-08-23 06:19:45 +0530

    Create 14.py

---
+++ 14.py
@@ -0,0 +1,33 @@
+# Classes / OOPS --> object oriented programming language
+
+# CAR --> blueprint
+#     --> Engine, brakes, tires, body
+
+
+# Mustag GT --> V8, mambo brakes, Michellin, Muscular
+# S class --> V6, nurek brakes, Michellin, Sedan
+# Escalade --> V8, Mambo brakes, Apolo, SUV
+# class CAR:
+#   def s    # attributes / objects
+#   def production(this):
+#       print(this.Engine)
+#       print(this.breaks)
+#       print(this.tires)
+#       print(this.body)
+
+# Mustang = CAR("V8","Mambo","Michellin","Muscular")
+# Mustang.production()
+# # I our real wold
+# # variable = 8
+
+# reverse a string 
+#  John --> nhoj
+
+
+# print only odd index elements of list
+list = [1,2,3,4,5]
+# OUTPUT as they are odd index numbers
+3 #---> #[2]
+4 #---> #[3]
+
+# REVISE the OOPS
