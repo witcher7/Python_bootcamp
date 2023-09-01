a18aaac489cfcc6b582db9af5284f00dbe96fa16
Rishabh Gulati <gulati.r7121@gmail.com>
2023-09-01 06:21:29 +0530

    Create 19.py

---
+++ 19.py
@@ -0,0 +1,46 @@
+# DATA SCIENCE --> the jobs are really good in data science and AI
+
+# NUMPY
+# it is a python library which helps us in making two dimensional arrays 
+
+# list = [1,2,3,4,5]
+
+# numpy = [[1,2,3,4,5] , [6,7,8,9,10]]
+
+
+# import numpy as np # as __ you can give any name that you like
+
+# we are importing a book / module which will help us in doing functions
+
+                                     # 
+# food = np.array([[["Coke", "Fanta","Redbull","Sprite"], ["Burgur","Fries","Pizza","Chocolate"]],[["Football","Basketball","Tennis","Soccer"],["Ball","Basketball Court","Tennis Court", "Field"]]] )
+# print(food)
+# print(type(food))
+import numpy as np
+
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+
+# | |
+# print(b[0])
+# list = [1,2,3,4,5]
+# print(list[0])
+
+
+# #               0   1  2    0  1  2
+# c = np.array([[1, 2, 3], [4, 5, 6]])
+# #                   0         1
+
+
+# print(c[1,1])
+ # go inside c array and find 0th array, find 1st element
+
+
+
+
+
+                   0.0       0.1           1.0       1.1
+                0.0.0
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+#                         0                      1
+print(d[0,1,1])
