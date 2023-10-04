8e0df8783b707a68bd4c17ca7696735524dce3fd
Rishabh Gulati <gulati.r7121@gmail.com>
2023-10-04 19:14:22 +0530

    Create 22.py

---
+++ 22.py
@@ -0,0 +1,23 @@
+#   list.append(take)
+
+# cookies = 0
+# for i in range(n):
+#   cookies = cookies+1
+
+# print(cookies)
+
+# # # give back number of elements
+# # INPUT --> [10,20,30,40,50]
+# # OUTPUT --> 5
+
+
+#You have to take input from the user and one more input from the user and find the maximum and minimum element inside the list
+
+
+list = [10,-10,3,0,100,345]
+
+# # OUTPUT
+# max = 345
+# min = -10
+
+
