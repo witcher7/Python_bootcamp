dcc64cc6d28461d0e98d66a88512197ab96f77cc
Rishabh Gulati <gulati.r7121@gmail.com>
2023-10-05 23:53:51 +0530

    Create file..py

---
+++ file..py
@@ -0,0 +1,72 @@
+# # # print(100)
+
+# # # DIFFERENT TYPES OF DATA WE CAN USE IN PYTHON
+
+# # #.1 INTEGERS --> NUMBERS FROM -ve to +ve 
+# # # -100,-4, 10,85,1000,200023 etc
+# # # we dont need " " or ' ' to write numbers
+# # print(type(200))
+
+# # #. STRINGS 
+# # # ALPHABETS, SYMBOLS, CHARACTERS
+# # # RIsh, Ugo%, Kangeshan$ ,&, A, apple
+# # # these words, alphabets,symbols will be inside " " or ' '
+
+# # print(type("HelloWorld!"))
+
+# # # FLOAT --> numbers which have decimals
+# # # 10.01, 0.01, 5.0003, 5.555 etc
+# # # dont need " " or ' 'to write floating point numbers
+
+# # print(type(100.04))
+
+# # # BOOLEAN / BOOL
+# # # in this one we have, TRUE OR FALSE
+# # print(type(True))
+# # print(type(False))
+
+# # VARIABLES --> the storage buckets which we use to store data
+
+# cookies = 100
+# print(cookies)
+
+# # NOW IF I WANT TO TAKE INPUT FROM THE USER
+
+
+# # this type of input will be considered for integers
+
+# anything = int(input("Enter your data : "))
+# print(anything)
+
+# # INPUT FOR STRINGS
+# text = str(input("Enter your data : "))
+# print(text)
+
+# # take input from the user for two values
+# # swap the values ( swap the variables and print them)
+
+
+# num1 = int(input("Enter number1 : "))
+# num2 = int(input("Enter number2 : "))
+
+# temp =num1
+# num1 = num2
+# num2 = temp
+
+# print("The value of num1 after SWAPPING is : ",num1)
+# print("The Value of num2 is SWAPPING : ",num2)
+
+
+# print(100+100) #200
+# print(100-100) #0
+# print(100*100)# 10000
+# print(100/100) # 1
+# print(100%100) # remainder --> 0
+
+print(10//2)
+# / --> decimal division
+# // --> integer division
+
+
+# TAKE INPUT FROM THE USER ( TWO VALUES)
+# DO THE OPERATIONS
