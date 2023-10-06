89656ba500508b7348844dd53b85b208b1decb64
Rishabh Gulati <gulati.r7121@gmail.com>
2023-10-06 09:15:40 +0530

    Create n-class.py

---
+++ n-class.py
@@ -0,0 +1,18 @@
+
+n = int(input("Enter your number: "))
+if n >100:
+  print("It is greater than hundred")
+elif n ==100: 
+  print("It is equal to hundred")
+else:
+  print("It is less than hundered")
+
+
+
+# = to assign
+# == to check 
+# COMPARISON OPERATION
+# > --> greater than
+# < --> less than
+# >= --> greater than equal to
+# <= --> less than equal to
